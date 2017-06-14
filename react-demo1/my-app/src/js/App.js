8acb5f9da7123f03f8747e5a2c63dcb31c121f6a
peng307git <2324964369@qq.com>
2017-06-14 15:24:31 +0800

    用create-react-app 快速构建react 单页面

--- react-demo1/my-app/src/js/App.js
+++ react-demo1/my-app/src/js/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../images/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
+  handleClicl() {
+      window.location = 'one.html';
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +15,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> aname
+          To get started, edit <code onClick={this.handleClicl}>src/App.js</code> aname
         </p>
       </div>
     );
--- react-demo1/my-app/src/one.js
+++ react-demo1/my-app/src/one.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './js/header.jsx';
 import Footer from './js/footer.jsx';
-import App from './js/App';
-import registerServiceWorker from './registerServiceWorker';
 import './css/common.css';
-import './css/index.css';
-
-
+import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
     <section>
         <Header />
-        <App />
         <Footer/>
     </section>,
-    document.getElementById('root')
+    document.getElementById('one')
 );
 registerServiceWorker();
